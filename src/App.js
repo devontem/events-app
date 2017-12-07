75dc809937899dc87142d308285712b194e81303
Devonte MacGlashan <devontem@yahoo-inc.com>
2017-12-06 19:22:43 -0800

    updated routes and added switch component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { createStore } from 'redux';
 import logo from './logo.svg';
 import './App.css';
-import ListviewContainer from './containters/Listview';
+import ListviewContainer from './containers/Listview/Listview';
+import DetailviewContainer from './containers/Detailview/Detailview';
+import store from './containers/store';
 
 class App extends Component {
   render() {
     return (
-      <ListviewContainer />
+      <Provider store={store}>
+
+        <BrowserRouter>
+          <Switch>
+          <Route exact path="/" component={ListviewContainer} />
+          <Route path="/detail/:id" component={DetailviewContainer} />
+          </Switch>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }

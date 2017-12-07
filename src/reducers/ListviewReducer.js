2fcb923675052703290ce49ecdc5c02db5216afb
Devonte MacGlashan <devontem@yahoo-inc.com>
2017-12-06 20:12:55 -0800

    updated the listview reducer

--- src/reducers/ListviewReducer.js
+++ src/reducers/ListviewReducer.js
@@ -1,8 +1,13 @@
-
 function listview(state = {}, action){
 	switch(action.type){
-		case 'GET_DATA':
+		case 'GET_LIST_DATA':
 			return state;
+		case 'GET_LIST_DATA_PENDING':
+			return {...state, pending: true };
+		case 'GET_LIST_DATA_FULFILLED':
+			return { data: action.payload.data.events, pending: false };
+		case 'GET_LIST_DATA_REJECTED':
+			return { data: [], error: true, message: 'There was an error fetching events' };
 		default:
 			return state;
 	}
